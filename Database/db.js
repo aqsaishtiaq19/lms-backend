0df123c27854540eec40f30ad63a52eccd7843d9
Aqsa Ishtiaq <aqsaishtiaq19@gmail.com>
2026-06-17 06:38:55 +0500

    updated database connection

--- Database/db.js
+++ Database/db.js
@@ -1,25 +1,16 @@
-
-
 const mongoose = require('mongoose');
+
 mongoose.set("strictQuery", false);
-mongoose.pluralize(null)
-// Connection of mongo db
+mongoose.pluralize(null);
+
 const connectDB = async () => {
-    await mongoose.connect('mongodb://127.0.0.1:27017/LMS-DB')
-    .then(() => console.log('Database Connected!'))
-    .catch((error) => {
-        console.log("DB Error" + error)
-    })
-}
+  await mongoose.connect(
+    "mongodb://aqsaishtiaq19_db_user:Zombie1919@ac-wa4atm1-shard-00-00.iuqnom3.mongodb.net:27017,ac-wa4atm1-shard-00-01.iuqnom3.mongodb.net:27017,ac-wa4atm1-shard-00-02.iuqnom3.mongodb.net:27017/LMS-backend?ssl=true&replicaSet=atlas-8ef1bz-shard-0&authSource=admin&retryWrites=true&w=majority"
+  )
+  .then(() => console.log(' Database Connected!'))
+  .catch((error) => {
+    console.log("DB Error: " + error);
+  });
+};
 
-module.exports = connectDB
-
-
-
-
-
-
-
-
-
-
+module.exports = connectDB;
