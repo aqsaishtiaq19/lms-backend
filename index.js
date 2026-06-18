58f9f5c298b006314368c74c0cbddbc074ffcb55
Aqsa Ishtiaq <aqsaishtiaq19@gmail.com>
2026-06-18 05:14:45 +0500

    fix: allow all Vercel URLs in CORS

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ const connectDB = require('./Database/db');
 connectDB();
 
 const app = express();
-
 app.use(cors({
-    origin: "https://lms-frontend-git-main-momin2.vercel.app",
+    origin: [
+        "https://lms-frontend-momin2.vercel.app",
+        "https://lms-frontend-git-main-momin2.vercel.app",
+        "https://lms-frontend-44t3bg55k-momin2.vercel.app"
+    ],
     credentials: true
 }));
 
