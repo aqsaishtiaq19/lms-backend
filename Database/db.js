0cf0962f26493f773c37a5285b603a996d1eccb8
Aqsa Ishtiaq <aqsaishtiaq19@gmail.com>
2026-07-19 05:06:36 +0500

    Fix undefined res bug in db connection error handler

--- Database/db.js
+++ Database/db.js
@@ -5,12 +5,10 @@ mongoose.set("strictQuery", false);
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log(" Database Connected!");
+    console.log("Database Connected!");
+  } catch (error) {
+    console.error("DATABASE CONNECTION ERROR:", error);
   }
-   catch (error) {
-    console.error("REGISTER ERROR:", error) 
-    res.status(500).json({ message: error.message }) 
-}
 };
 
 module.exports = connectDB;
